4dc1346a36a9e3b884b432764af744b2c04ae55f
Ethanos7405! <st10440914@vcconnect.edu.za>
2025-09-17 10:30:40 +0200

    Add project files.

---
+++ PROG6212POE/Controllers/ClaimController.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PROG6212POE.Models;
+using PROG6212POE.Services;
+
+namespace PROG6212POE.Controllers
+{
+    public class ClaimController : Controller
+    {
+        private readonly ClaimTableService _tablestorage;
+
+        public ClaimController(ClaimTableService tablestorage)
+        {
+            _tablestorage = tablestorage;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var claims = await _tablestorage.GetAllClaimsAsync();
+            return View(claims);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Claim model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _tablestorage.InsertClaimAsync(model);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(model);
+        }
+    }
+}
---
+++ PROG6212POE/Models/Claim.cs
@@ -0,0 +1,19 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+
+namespace PROG6212POE.Models
+{
+    public class Claim : ITableEntity
+    {
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+        public string ClaimId { get; set; }
+        public string LecturerName { get; set; }
+        public string SalaryMonth { get; set; }
+        public string HoursWorked { get; set; }
+    }
+}
